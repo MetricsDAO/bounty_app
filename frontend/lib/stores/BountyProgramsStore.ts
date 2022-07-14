45781a492b4cc8fa0a1b80f53ee19dd26d6ff209
kakamora <86695148+thekakamora@users.noreply.github.com>
2022-07-13 19:13:40 -0700

    Update BountyProgramsStore.ts

--- frontend/lib/stores/BountyProgramsStore.ts
+++ frontend/lib/stores/BountyProgramsStore.ts
@@ -12,25 +12,11 @@ export interface BountyProgramState {
 }
 
 const programs = [
-  {
-    name: "Harmony",
-    slug: "harmony",
-    bountyProgramID: "16b49c12-cf07-0df5-c5b0-c7cdcfe12eb5", // canny id
-  },
+  
   {
     name: "Aave v3",
     slug: "aave-v3",
     bountyProgramID: "9f550aff-5d3e-053a-8034-33df6573d9cd",
-  },
-  {
-    name: "Biconomy",
-    slug: "biconomy",
-    bountyProgramID: "c1f62581-dc7b-125a-6f35-1bfb0ee89215",
-  },
-  {
-    name: "Bancor v3",
-    slug: "bancor-v3",
-    bountyProgramID: "588a1f50-40f3-3b23-be33-35c49a39721f",
   },
   {
     name: "True Freeze",
@@ -38,20 +24,10 @@ const programs = [
     bountyProgramID: "24c602b5-a202-9c05-4465-5b0d37132401",
   },
   {
-    name: "Nouns DAO",
-    slug: "nouns-dao",
-    bountyProgramID: "fad83cf2-c77d-9d46-2e7e-744ee3fa8bfe",
-  },
-  {
-    name: "UMA Protocol",
-    slug: "uma-protocol",
-    bountyProgramID: "6f693a02-7be2-5698-6feb-989a2d244d1c",
-  },
-  {
-    name: "NEAR",
-    slug: "near",
-    bountyProgramID: "fad867cb-c0d2-79ce-c961-906062903f45",
-  },
+    name: "Hacks, Scandals and Scams",
+    slug: "hacks-scandals-and-scams",
+    bountyProgramID: "08274d93-6dba-cba6-5e27-926674c774ec",
+  }
 ];
 
 export const useBountyProgramStore = create<BountyProgramState>((set: any) => ({
