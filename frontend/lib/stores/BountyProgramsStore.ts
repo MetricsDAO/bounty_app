ba0ffdef03233efcbb209c6f015cf6d0ca425f85
Hannah Lerman <43100701+thehannahlerman@users.noreply.github.com>
2022-09-15 16:51:41 -0400

    add plagiarism program and get rid of example stuff

--- frontend/lib/stores/BountyProgramsStore.ts
+++ frontend/lib/stores/BountyProgramsStore.ts
@@ -41,11 +41,15 @@ const programs = [
     name: "Goldfinch",
     slug: "goldfinch",
     bountyProgramID: "c1af1118-3163-a6b3-4b67-fc73ef7ff008",
-    customTitle: "This is My Custom Title for Goldfinch",
+  },
+  {
+    name: "Report Plagiarism",
+    slug: "report-submission",
+    bountyProgramID: "49927eef-fe6e-c39b-f1e6-012ab4bb9a2d",
+    customTitle: "Report Plagiarized Submissions",
     customDescription:
-      "This is My Custom Description for Goldfinch 🚀 Try a really long sentence and insert paragraph breaks how they will appear to see if this really works or if we need to insert breaks ourself.\n\nHere is a second paragraph.",
-    customHint: "This is My Custom Hint for Goldfinch 👀",
-    hintLink: "https://metricsdao.xyz/",
+      "Is your work plagiarized by another analyst? Please use the form below and report them.",
+    disableHint: true,
   },
 ];
 
